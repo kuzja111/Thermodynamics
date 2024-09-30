1ee833802a8ca87cf2175062c48519e72dad41ae
Emma Smith Zbarsky <esmithzb@mathworks.com>
2024-09-30 15:55:25 -0400

    Add expected error for 24a BuildRefrigerator

--- SoftwareTests/PreFiles/PreBuildRefrigerator.m
+++ SoftwareTests/PreFiles/PreBuildRefrigerator.m
@@ -1,4 +1,4 @@
 %  Pre-run script for BuildRefrigerator.mlx
 % ---- Known Issues     -----
-KnownIssuesID = "physmod:common:simtypes:assert:Assertion";
+KnownIssuesID = ["physmod:common:simtypes:assert:Assertion","physmod:simscape:simtypes:assert:Assertion"];
 % ---- Pre-run commands -----
