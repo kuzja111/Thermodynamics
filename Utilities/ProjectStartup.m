4a4a503b8865182fe52c38f14aaf900d8d093a43
Emma Smith Zbarsky <esmithzb@mathworks.com>
2024-09-30 15:27:05 -0400

    Adjust details for release
    * Update the ci.yml with the required products.
    * Update the ProjectStartup.m to match the new version requirements.
    * Update the README.md to not look for unmaintained CI badges

--- Utilities/ProjectStartup.m
+++ Utilities/ProjectStartup.m
@@ -2,10 +2,10 @@ function ProjectStartup
 % Set up check for version number
 releaseInfo = matlabRelease;
 str = "This is release " + releaseInfo.Release + ",";
-if isMATLABReleaseOlderThan("R2022b")
-    disp(str + " so it may not run this example which was built in R2022b.")
+if isMATLABReleaseOlderThan("R2024a")
+    disp(str + " so it may not run this example which was built in R2024a.")
 else
-    disp(str + " and this was designed using R2022b.")
+    disp(str + " and this was designed using R2024a.")
     disp("If you find errors, please report them to onlineteaching@mathworks.com")
 end
 
