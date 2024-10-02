f33d5085952d6ec48fc476dc60ed43335d40f98f
Valentin Boutrouche <114016079+vboutrou@users.noreply.github.com>
2024-10-02 13:25:19 -0400

    Update CheckTestResults.m

--- SoftwareTests/CheckTestResults.m
+++ SoftwareTests/CheckTestResults.m
@@ -16,7 +16,7 @@ classdef CheckTestResults < matlab.unittest.TestCase
 
         function Version = GetResults(Project)
             RootFolder = currentProject().RootFolder;
-            Version = dir(fullfile(RootFolder,"SoftwareTests","TestResults*.txt"));
+            Version = dir(fullfile(RootFolder,"public","TestResults*.txt"));
             Version = extractBetween([Version.name],"TestResults_",".txt");
         end
 
@@ -37,7 +37,7 @@ classdef CheckTestResults < matlab.unittest.TestCase
     methods(Test)
 
         function CheckResults(testCase,Version)
-            File = fullfile("SoftwareTests","TestResults_"+Version+".txt");
+            File = fullfile("public","TestResults_"+Version+".txt");
             Results = readtable(File,TextType="string");
             testCase.verifyTrue(all(Results.Passed));
         end
